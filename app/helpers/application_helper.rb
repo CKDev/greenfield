43ebebbe55f38824dc515f947ccfcdbd1429c08f
Alex Brinkman <abrink@commercekitchen.com>
2016-08-29 22:04:46 -0600

    [CHG] Add local_time config, active_link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def add_active_link(paths, classes = "")
+    paths.each do |path|
+      if current_page? path
+        return "#{classes} active"
+      end
+    end
+    classes
+  end
+
+  def tel_to(number)
+    link_to number, "tel:#{number}"
+  end
+
 end
